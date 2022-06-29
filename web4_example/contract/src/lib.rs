e5d71a2c94a4e9b2be0329810ef599dee5017748
Wabinab <71509699+Wabinab@users.noreply.github.com>
2022-06-29 07:05:09 +0000

    skeleton done

---
+++ web4_example/contract/src/lib.rs
@@ -0,0 +1,12 @@
+mod web4;
+
+
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{near_bindgen, AccountId};
+
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, Default)]
+pub struct Contract {
+}
