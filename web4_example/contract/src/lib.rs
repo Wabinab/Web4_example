da3f11d672fda1bbd88a897c16e608e2dbe0248d
Wabinab <71509699+Wabinab@users.noreply.github.com>
2022-06-30 07:30:11 +0000

    header and footer done

--- web4_example/contract/src/lib.rs
+++ web4_example/contract/src/lib.rs
@@ -9,6 +9,9 @@ use near_sdk::{near_bindgen};
 
 const INDEX_BODY: &str = include_str!("./design/index.html");
 const CSS_BODY: &str = include_str!("./design/css.html");
+const IMPORTMAP_BODY: &str = include_str!("./design/importmap.html");
+const JS_BODY: &str = include_str!("./design/js.html");
+const BODY_JS_BODY: &str = include_str!("./design/body_js.html");
 
 
 #[near_bindgen]
@@ -30,6 +33,9 @@ impl Contract {
       Web4Response::html_response(
         INDEX_BODY
             .replace("%STYLE%", CSS_BODY)
+            .replace("%IMPORTMAP%", IMPORTMAP_BODY)
+            .replace("%SCRIPT%", JS_BODY)
+            .replace("%BODY_JS%", BODY_JS_BODY)
             .to_owned()
       )
     }
