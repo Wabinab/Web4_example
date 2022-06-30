88e09f59e78b18c52b415a4b44c7909d148b2ec5
Wabinab <71509699+Wabinab@users.noreply.github.com>
2022-06-30 08:21:04 +0000

    refactoring complete with an about page

--- web4_example/contract/src/lib.rs
+++ web4_example/contract/src/lib.rs
@@ -1,17 +1,26 @@
 mod web4;
+mod utils;
 
 use crate::web4::*;
+use crate::utils::*;
 
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::{near_bindgen};
 
 
+const HEAD_BODY: &str = include_str!("./design/shared/head.html");
+const CSS_BODY: &str = include_str!("./design/shared/css.html");
+const IMPORTMAP_BODY: &str = include_str!("./design/shared/importmap.html");
+const JS_BODY: &str = include_str!("./design/shared/js.html");
+const NAVBAR_BODY: &str = include_str!("./design/shared/navbar.html");
+const FOOTER_BODY: &str = include_str!("./design/shared/footer.html");
+
+const LOGIN_CONTROLLER_BODY: &str = include_str!("./design/controller/login_controller.html");
+
+const ABOUT_BODY: &str = include_str!("./design/about.html");
 const INDEX_BODY: &str = include_str!("./design/index.html");
-const CSS_BODY: &str = include_str!("./design/css.html");
-const IMPORTMAP_BODY: &str = include_str!("./design/importmap.html");
-const JS_BODY: &str = include_str!("./design/js.html");
-const BODY_JS_BODY: &str = include_str!("./design/body_js.html");
+
 
 
 #[near_bindgen]
@@ -25,18 +34,20 @@ impl Contract {
     #[allow(unused_variables)]
     pub fn web4_get(&self, request: Web4Request) -> Web4Response {
       let path = request.path.expect("Path expected.");
+      let splitted_path = path.split("/").collect::<Vec<&str>>();
 
       if path == "/robots.txt" {
         return Web4Response::plain_response("User-agent: *\nDisallow:".to_owned());
       }
 
+      if path == "/about" {
+        return Web4Response::html_response(
+          common_page_template(ABOUT_BODY)
+        );
+      }
+
       Web4Response::html_response(
-        INDEX_BODY
-            .replace("%STYLE%", CSS_BODY)
-            .replace("%IMPORTMAP%", IMPORTMAP_BODY)
-            .replace("%SCRIPT%", JS_BODY)
-            .replace("%BODY_JS%", BODY_JS_BODY)
-            .to_owned()
+        common_page_template(INDEX_BODY)
       )
     }
 }
